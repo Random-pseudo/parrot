92579e3d5530e5c0552d8ec40cc6159fd1e63da8
Random-pseudo <152723003+Random-pseudo@users.noreply.github.com>
2025-02-24 00:44:51 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
 set REG_KEY="HKCU\Software\Microsoft\Windows\CurrentVersion\Run"
-set FILE_PATH="C:\Utilisateurs\Eleve\Téléchargements\parrot_background.bat"
+set FILE_PATH="C:\Utilisateurs\Eleve\parrot\parrot_background.bat"
 
 reg add %REG_KEY% /v Parrot /t REG_SZ /d "%FILE_PATH%" /f
 

76e1bdbce64271ec0593c169702ea2891bee3a06
Random-pseudo <152723003+Random-pseudo@users.noreply.github.com>
2025-02-24 00:39:46 +0100

    Update install_parrot.bat

--- install_parrot.bat
+++ install_parrot.bat
@@ -1,12 +1,20 @@
 @echo off
 setlocal
 
-curl -L -o cleanup.bat https://raw.githubusercontent.com/Random-pseudo/parrot/refs/heads/main/cleanup.bat
-curl -L -o setup.bat https://raw.githubusercontent.com/Random-pseudo/parrot/refs/heads/main/setup.bat
-curl -L -o loop_parrot.bat https://raw.githubusercontent.com/Random-pseudo/parrot/refs/heads/main/loop_parrot.bat
-curl -L -o parrot_background.bat https://raw.githubusercontent.com/Random-pseudo/parrot/refs/heads/main/parrot_background.bat
+:: Définir le chemin du dossier "parrot"
+set PARROT_DIR=%USERPROFILE%\parrot
 
-call setup.bat
-start parrot_background.bat
+:: Créer le dossier "parrot" s'il n'existe pas
+if not exist "%PARROT_DIR%" mkdir "%PARROT_DIR%"
+
+:: Télécharger les fichiers dans le dossier "parrot"
+curl -L -o "%PARROT_DIR%\cleanup.bat" https://raw.githubusercontent.com/Random-pseudo/parrot/refs/heads/main/cleanup.bat
+curl -L -o "%PARROT_DIR%\setup.bat" https://raw.githubusercontent.com/Random-pseudo/parrot/refs/heads/main/setup.bat
+curl -L -o "%PARROT_DIR%\loop_parrot.bat" https://raw.githubusercontent.com/Random-pseudo/parrot/refs/heads/main/loop_parrot.bat
+curl -L -o "%PARROT_DIR%\parrot_background.bat" https://raw.githubusercontent.com/Random-pseudo/parrot/refs/heads/main/parrot_background.bat
+
+:: Exécuter setup.bat et lancer parrot_background.bat
+call "%PARROT_DIR%\setup.bat"
+start "" "%PARROT_DIR%\parrot_background.bat"
 
 exit

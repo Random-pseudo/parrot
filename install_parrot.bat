0e9f639050dcfe50f12b6f8d87e364fd9adb6b35
Random-pseudo <152723003+Random-pseudo@users.noreply.github.com>
2025-02-23 23:39:19 +0100

    Update install_parrot.bat

--- install_parrot.bat
+++ install_parrot.bat
@@ -1,16 +1,16 @@
 @echo off
 setlocal
 
-set REPO_URL=https://raw.githubusercontent.com/Random-pseudo
-/parrot/main
+set REPO_URL=https://raw.githubusercontent.com/Random-pseudo/repository/main
+set PARROT_DIR=%USERPROFILE%\parrot
 
-curl -L -o loop_parrot.bat %REPO_URL%/loop_parrot.bat
-curl -L -o parrot_background.bat %REPO_URL%/parrot_background.bat
-curl -L -o setup.bat %REPO_URL%/setup.bat
-curl -L -o cleanup.bat %REPO_URL%/cleanup.bat
+if not exist "%PARROT_DIR%" mkdir "%PARROT_DIR%"
 
+curl -L -o "%PARROT_DIR%\loop_parrot.bat" %REPO_URL%/loop_parrot.bat
+curl -L -o "%PARROT_DIR%\parrot_background.bat" %REPO_URL%/parrot_background.bat
+curl -L -o "%PARROT_DIR%\setup.bat" %REPO_URL%/setup.bat
 
-
+cd /d "%PARROT_DIR%"
 call setup.bat
 start parrot_background.bat
 

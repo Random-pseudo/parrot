bc5e9dcf3fb6e774290af55086ba3bd1e939b022
Random-pseudo <152723003+Random-pseudo@users.noreply.github.com>
2025-02-24 00:27:43 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,4 +4,5 @@ set FILE_PATH="C:\Utilisateurs\Eleve\Téléchargements\parrot_background.bat"
 
 reg add %REG_KEY% /v Parrot /t REG_SZ /d "%FILE_PATH%" /f
 
+echo Le script Parrot a été ajouté au démarrage !
 exit

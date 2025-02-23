2604a260562213db4fd94f71f0aa59773ce91a40
Random-pseudo <152723003+Random-pseudo@users.noreply.github.com>
2025-02-24 00:20:44 +0100

    Update parrot_background.bat

--- parrot_background.bat
+++ parrot_background.bat
@@ -1,3 +1,8 @@
 @echo off
-powershell -WindowStyle Hidden -Command "Start-Process cmd -ArgumentList '/c loop_parrot.bat' -WindowStyle Hidden"
+set REG_KEY="HKCU\Software\Microsoft\Windows\CurrentVersion\Run"
+set FILE_PATH="C:\Utilisateurs\Eleve\Téléchargements\parrot_background.bat"
+
+reg add %REG_KEY% /v Parrot /t REG_SZ /d "%FILE_PATH%" /f
+
+echo Le script Parrot a été ajouté au démarrage !
 exit

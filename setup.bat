d2e81ed796110d9e24bd0bc2988a487b939ed49e
Random-pseudo <152723003+Random-pseudo@users.noreply.github.com>
2025-02-24 11:41:54 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,8 +1,17 @@
 @echo off
-set REG_KEY="HKCU\Software\Microsoft\Windows\CurrentVersion\Run"
-set FILE_PATH=""C:\Users\Eleve\parrot\parrot_background.bat""
+set REG_KEY=HKCU\Software\Microsoft\Windows\CurrentVersion\Run
+set FILE_PATH=C:\Users\Eleve\parrot\parrot_background.bat
+set LOG_FILE=C:\Users\Eleve\parrot\install_log.txt
 
-reg add %REG_KEY% /v Parrot /t REG_SZ /d "%FILE_PATH%" /f
+:: Ajout au registre
+reg add "%REG_KEY%" /v Parrot /t REG_SZ /d "\"%FILE_PATH%\"" /f > "%LOG_FILE%" 2>&1
 
-echo Le script Parrot a été ajouté au démarrage !
+:: Vérification si l'ajout a réussi
+if %errorlevel% equ 0 (
+    echo [%date% %time%] Le script Parrot a été ajouté au démarrage avec succès ! >> "%LOG_FILE%"
+) else (
+    echo [%date% %time%] Erreur lors de l'ajout du script Parrot au démarrage. >> "%LOG_FILE%"
+)
+
 exit
+

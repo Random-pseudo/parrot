7b46b21dd9cc4410c18011e3a71667c46cd44696
Random-pseudo <152723003+Random-pseudo@users.noreply.github.com>
2025-02-24 07:30:35 +0100

    Update loop_parrot.bat

--- loop_parrot.bat
+++ loop_parrot.bat
@@ -3,5 +3,11 @@ set PARROT_DIR=%USERPROFILE%\parrot
 
 :loop
 start cmd /k "curl parrot.live"
-timeout /t 15
+
+:: Générer un temps aléatoire entre 60 et 180 secondes
+set /a WAIT_TIME=(%RANDOM% %% 121) + 60
+
+:: Pause pour un temps aléatoire
+timeout /t %WAIT_TIME% /nobreak > nul
+
 goto loop

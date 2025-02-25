19b373710c90b48397a8fc52f3107a6a01f94bf5
Random-pseudo <152723003+Random-pseudo@users.noreply.github.com>
2025-02-25 13:38:06 +0100

    Update cleanup.bat

--- cleanup.bat
+++ cleanup.bat
@@ -1,12 +1,10 @@
-taskkill /F /IM wscript.exe >nul 2>&1
-taskkill /F /IM cmd.exe /T
-
-timeout /t 5 /nobreak >nul
-
-taskkill /F /IM cmd.exe /T
-
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Parrot /f
+taskkill /F /IM wscript.exe >nul 2>&1
+for /f "tokens=2 delims=," %A in ('tasklist /FI "IMAGENAME eq cmd.exe" /FO CSV /NH') do if not "%A"=="%PID%" taskkill /F /PID %A
 
 
 del /F /Q "C:\Users\Eleve\install_parrot.bat"
 rd /S /Q "C:\Users\Eleve\parrot"
+
+exit
+

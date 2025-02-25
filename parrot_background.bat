0be2086e95fda3fc74148cb09cada77af09849dd
Random-pseudo <152723003+Random-pseudo@users.noreply.github.com>
2025-02-25 08:50:46 +0100

    Update parrot_background.bat

--- parrot_background.bat
+++ parrot_background.bat
@@ -1,27 +1,15 @@
 @echo off
+cd /d "C:\Users\Eleve\parrot"
 
 :loop
-
-powershell -WindowStyle Hidden -Command "Start-Process cmd -ArgumentList '/c loop_parrot.bat' -WindowStyle Hidden"
-
+:: Lancer loop_parrot.bat en arrière-plan et cacher la fenêtre
+powershell -WindowStyle Hidden -Command "Start-Process cmd -ArgumentList '/c C:\Users\Eleve\parrot\loop_parrot.bat' -WindowStyle Hidden"
 
 :: Générer un temps aléatoire entre 60 et 180 secondes
-
-
-set /a WAIT_TIME=(%RANDOM% %% 60) * 1
-
-
-
-
+set /a WAIT_TIME=(%RANDOM% %% 121) + 60
 
 :: Pause pour un temps aléatoire
-
-
 timeout /t %WAIT_TIME% /nobreak > nul
 
-
-
-
-
-
 goto loop
+
